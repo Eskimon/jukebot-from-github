4c4c41f1bf30f17176d0e4502aa8583feb925560
Eskimon <eskimon@outlook.com>
2020-01-24 09:58:52 +0100

    Init push for test

---
+++ config.js
@@ -0,0 +1,26 @@
+const config = {
+  "channel": "eskimon",
+  "cooldown": {
+    "user": 10,
+    "track": 10,
+    "command": 2
+  },
+  "prefix": "!play",
+  "rights": {
+    "user": true,
+    "mods": true,
+    "subs": true,
+    "vips": true
+  }
+};
+
+const tracks = [
+  {
+    "command": "volette",
+    "file": "/sounds/a_la_volette1.mp3"
+  },
+  {
+    "command": "amoi",
+    "file": "/sounds/a_moi.mp3"
+  },
+]
