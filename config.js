4a49562c49e5728caf25a1c4c6ca6b3861b804dc
Eskimon <eskimon@outlook.com>
2020-01-24 10:26:36 +0100

    Ajoute des pistes à la config

--- config.js
+++ config.js
@@ -16,11 +16,35 @@ const config = {
 
 const tracks = [
   {
-    "command": "volette",
-    "file": "./sounds/a_la_volette1.mp3"
+  "command": "alavolette",
+  "file": "./sounds/a_la_volette1.mp3"
   },
   {
-    "command": "amoi",
-    "file": "./sounds/a_moi.mp3"
+  "command": "amoi",
+  "file": "./sounds/a_moi.mp3"
   },
+  {
+  "command": "rienacarer",
+  "file": "./sounds/rien_a_carer.mp3"
+  },
+  {
+  "command": "sortezvouslesdoigts",
+  "file": "./sounds/sortez-vous_les_doigts_du_cu3"
+  },
+  {
+  "command": "grosnul",
+  "file": "./sounds/soupcon_gros_nul.mp3"
+  },
+  {
+  "command": "cestpro",
+  "file": "./sounds/voila_cest_pro.mp3"
+  },
+  {
+  "command": "pastille",
+  "file": "./sounds/vous_la_crachez_votre_pastille.mp3"
+  },
+  {
+  "command": "vousrigolez",
+  "file": "./sounds/vous_rigolez_jespere.m3"
+  }
 ];
